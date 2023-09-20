3bb5c5f65f47cde24b128ed234d46e2dba233b75
Mateusz Paprocki <mattpap@gmail.com>
2023-09-20 12:57:41 +0200

    Add a backwards compatibility layer

--- src/bokeh/models/glyphs.py
+++ src/bokeh/models/glyphs.py
@@ -113,7 +113,7 @@ __all__ = (
     'AnnularWedge',
     'Annulus',
     'Arc',
-    'Band',
+    #'Band',
     'Bezier',
     'Block',
     'Circle',
@@ -152,7 +152,7 @@ __all__ = (
     'VSpan',
     'VStrip',
     'Wedge',
-    'Whisker',
+    #'Whisker',
     'XYGlyph',
 )
 
